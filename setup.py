d074fdb488a730ba8a42b6ecafb4a1d7f30d5a82
Brian Graham <brian@statagroup.com>
2018-06-02 18:36:54 +0200

    Bump version for sake of URLs ponting to each-other

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "studio_ghibli_api_sdk"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 # To install the library, run the following
 #
 # python setup.py install
