ca1908c4758a8895fe7c31be7a8e90a0cc24eb83
Brian Graham <brian@statagroup.com>
2018-06-02 19:59:17 +0200

    These were also wrong'

--- studio_ghibli_api_sdk/api/species_api.py
+++ studio_ghibli_api_sdk/api/species_api.py
@@ -219,7 +219,7 @@ class SpeciesApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='list[Species]',  # noqa: E501
+            response_type='Species',  # noqa: E501
             auth_settings=auth_settings,
             async=params.get('async'),
             _return_http_data_only=params.get('_return_http_data_only'),
--- studio_ghibli_api_sdk/api/vehicles_api.py
+++ studio_ghibli_api_sdk/api/vehicles_api.py
@@ -219,7 +219,7 @@ class VehiclesApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='list[Vehicles]',  # noqa: E501
+            response_type='Vehicles',  # noqa: E501
             auth_settings=auth_settings,
             async=params.get('async'),
             _return_http_data_only=params.get('_return_http_data_only'),
